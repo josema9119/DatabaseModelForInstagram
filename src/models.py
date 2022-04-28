737a8ecb65e8646e5d2e990dd1192daf4cc4a893
josema9119 <jose.ma.mm.91@gmail.com>
2022-04-28 15:17:15 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -8,31 +8,38 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique= True)
+    follower = relationship("Follower")
+    post = relationship("Post")
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    
+class Comment(Base):
+    __tablename__="comment"
+    id = Column(Integer,primary_key=True)
+    text = Column(String(250), nullable=False)
+    user_id=Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Post)
 
-    def to_dict(self):
-        return {}
+class Follower(Base):
+    __tablename__='follower'
+    id = Column(Integer, primary_key=True)
+    follower = relationship(User)
+    following_id = Column(Integer, ForeignKey('user.id'))
+    following = relationship (User)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+render_er(Base, 'diagram.png')
